dc0b461c13d20de63dd556a7a28ea64bc427bfcf
Ubuntu <ubuntu@localhost.localdomain>
2025-08-01 11:40:13 +0800

    upload

--- faiss_engine/__init__.py
+++ faiss_engine/__init__.py
@@ -1,5 +1,4 @@
 # FAISS引擎包初始化文件
 from .embeddings import BGE_EmbeddingService
-from .vector_store import FAISSVectorStore
 
-__all__ = ["BGE_EmbeddingService", "FAISSVectorStore"]
+__all__ = ["BGE_EmbeddingService"]
